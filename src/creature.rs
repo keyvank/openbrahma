19d7170d1c3aaa90569f9ae94456fd5041c1300e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-03-28 16:11:26 +0430

    add health

--- src/creature.rs
+++ src/creature.rs
@@ -2,6 +2,17 @@ use super::brain::Brain;
 use super::world::Object;
 
 pub struct Creature<O: Object> {
+    health: u32,
     brain: Brain,
     body: O,
+}
+
+impl<O: Object> Creature<O> {
+    pub fn new(health: u32, brain: Brain, body: O) -> Creature<O> {
+        Creature {
+            health,
+            brain,
+            body,
+        }
+    }
 }

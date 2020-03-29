923c0763b39ba18f930f3618c80473126cbdc78d
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-03-29 17:22:14 +0430

    add and track motors

--- src/creature.rs
+++ src/creature.rs
@@ -6,23 +6,27 @@ pub struct Creature<O: Object> {
     brain: Brain,
     body: O,
     eye: Vec<NeuronId>,
+    motors: Vec<NeuronId>,
 }
 
 impl<O: Object> Creature<O> {
     pub fn new(health: u32, brain: Brain, body: O) -> Creature<O> {
         let eye = brain.random_neurons(10);
+        let motors = brain.random_neurons(4); // Forward, Backward, Rotate Left, Rotate Right
         Creature {
             health,
             brain,
             body,
             eye,
+            motors,
         }
     }
 
     pub fn tick(&mut self) {
         self.brain.tick();
         for &e in self.eye.iter() {
-            self.brain.stimulate(e, 0i32);
+            self.brain.stimulate(e, 3i32);
         }
+        let motor_deltas = self.brain.get_deltas(&self.motors);
     }
 }

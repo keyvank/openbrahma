e4639d0ecfb096db0069c04fbdcceb850fbc55be
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-03-29 18:01:09 +0430

    add food class

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod brain;
 mod creature;
+mod food;
 mod object;
 mod vector;
 mod world;
 
 pub use brain::*;
 pub use creature::*;
+pub use food::*;
 pub use object::*;
 pub use vector::*;
 pub use world::*;

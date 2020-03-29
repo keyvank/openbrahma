9ca450b4a3111277f74f1ad76736caddf9e1ade4
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-03-29 17:34:34 +0430

    change name

--- src/creature.rs
+++ src/creature.rs
@@ -1,5 +1,5 @@
 use super::brain::{Brain, NeuronId};
-use super::world::Object;
+use super::object::Object;
 
 pub struct Creature<O: Object> {
     health: u32,
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 mod brain;
 mod creature;
+mod object;
 mod vector;
-mod world;
 
 pub use brain::*;
 pub use creature::*;
+pub use object::*;
 pub use vector::*;
-pub use world::*;

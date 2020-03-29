d7c6f226e8e1378863cc6e3f8088f48eb1f1e6af
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-03-30 03:47:47 +0430

    add_object func

--- src/world.rs
+++ src/world.rs
@@ -9,6 +9,14 @@ pub struct World {
 }
 
 impl World {
+    pub fn new() -> World {
+        World {
+            objects: Vec::new(),
+        }
+    }
+    pub fn add_object(&mut self, o: Box<dyn Object>) {
+        self.objects.push(o);
+    }
     pub fn tick(&mut self) {
         for obj in self.objects.iter_mut() {
             obj.tick();

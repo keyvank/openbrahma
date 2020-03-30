945ea7ae975788936b8bf292d1ef4c864ea426bf
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-03-31 01:47:28 +0430

    add see function

--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,4 @@
-use super::shape::{Ray, Shape, Transform};
+use super::shape::{Intersection, Ray, Shape, Transform};
 use super::vector::Vector;
 
 #[derive(Debug)]
@@ -37,6 +37,27 @@ impl World {
     }
     pub fn add_object(&mut self, o: Object) {
         self.objects.push(o);
+    }
+    fn see(&mut self, r: Ray, fov: f64, res: i32) -> Vec<Option<Intersection>> {
+        let mut view = Vec::new();
+        for i in -(res / 2)..(res / 2) {
+            let ray = Ray {
+                pos: r.pos,
+                ang: r.ang + fov * (i as f64) / (res as f64),
+            };
+            view.push(
+                self.objects
+                    .iter()
+                    .map(|obj| obj.body.shape().intersects(&obj.trans, &ray))
+                    .filter_map(|opt| opt)
+                    .min_by(|a, b| {
+                        a.dist
+                            .partial_cmp(&b.dist)
+                            .unwrap_or(std::cmp::Ordering::Equal)
+                    }),
+            );
+        }
+        view
     }
     pub fn update(&mut self) {
         for obj in self.objects.iter_mut() {

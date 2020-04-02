c4f00bff60e7a8eb9932dfe803b5920e4d634526
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-02 16:58:58 +0430

    actuate return vec of actions

--- src/actuate.rs
+++ src/actuate.rs
@@ -1,13 +1,21 @@
 use super::shape::Transform;
 use super::world::{Object, Updatable, World};
 
+pub enum Action {
+    Create(Transform, Box<dyn Updatable>),
+    Delete(usize),
+    Put(usize, Transform),
+}
+
 pub trait Actuator {
-    fn actuate(&self, u: &Object, w: &mut World);
+    fn actuate(&self, u: &Object, w: &mut World) -> Vec<Action>;
 }
 
 pub struct Move {
     pub trans: Transform,
 }
 impl Actuator for Move {
-    fn actuate(&self, u: &Object, w: &mut World) {}
+    fn actuate(&self, u: &Object, w: &mut World) -> Vec<Action> {
+        Vec::new()
+    }
 }

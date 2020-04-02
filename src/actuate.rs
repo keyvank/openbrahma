36567eeea75f282de6becae3b8d6842a9775edb0
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-04-02 16:59:47 +0430

    doesnt need mutable world

--- src/actuate.rs
+++ src/actuate.rs
@@ -8,14 +8,14 @@ pub enum Action {
 }
 
 pub trait Actuator {
-    fn actuate(&self, u: &Object, w: &mut World) -> Vec<Action>;
+    fn actuate(&self, u: &Object, w: &World) -> Vec<Action>;
 }
 
 pub struct Move {
     pub trans: Transform,
 }
 impl Actuator for Move {
-    fn actuate(&self, u: &Object, w: &mut World) -> Vec<Action> {
+    fn actuate(&self, u: &Object, w: &World) -> Vec<Action> {
         Vec::new()
     }
 }
